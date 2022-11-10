6e873aa38dc503515fd8877666393b7e9548454d
maxmcnich <108849911+maxmcnich@users.noreply.github.com>
2022-11-10 11:01:11 -0800

    Update next.config.js

--- tailwindui-salient/next.config.js
+++ tailwindui-salient/next.config.js
@@ -6,24 +6,5 @@ const nextConfig = {
     newNextLinkBehavior: true,
     scrollRestoration: true,
   },
- const path = require("path");
-
-module.exports = {
-  trailingSlash: true,
-  webpackDevMiddleware: (config) => {
-    config.watchOptions = {
-      poll: 1000,
-      aggregateTimeout: 300,
-    };
-
-    return config;
-  },
-  sassOptions: {
-    includePaths: [path.join(__dirname, "styles")],
-  },
-  future: {
-    webpack5: true,
-  },
-};
 }
 module.exports = nextConfig
