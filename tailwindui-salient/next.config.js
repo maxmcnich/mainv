4e7cfd1f58677e481b230c965476e774ab6d2e77
maxmcnich <108849911+maxmcnich@users.noreply.github.com>
2022-11-10 10:56:33 -0800

    Update next.config.js

--- tailwindui-salient/next.config.js
+++ tailwindui-salient/next.config.js
@@ -2,11 +2,20 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  future: { webpack5: true },
   experimental: {
     newNextLinkBehavior: true,
     scrollRestoration: true,
   },
+  
+ const webpack = require("webpack");
+console.log(webpack.version); // 5.21.2
+module.exports = {
+  webpack: function (config, options) {
+    console.log(options.webpack.version); // 4.44.1
+    config.experiments = {};
+    return config;
+  },
+};
+  
 }
-
 module.exports = nextConfig
